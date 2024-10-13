41d81f9c0d5b542bd6d1cc2bef53bf366bff83f3
meriton <meriton@somewhere.org>
2024-10-14 01:02:16 +0200

    clean up indentation

--- src/app/coverage-indicator.component.ts
+++ src/app/coverage-indicator.component.ts
@@ -5,30 +5,30 @@ import { CoverageReport} from "./bedarf";
 	selector: 'coverage-indicator',
 	standalone: true,
 	styles: [`
-						hr {
-							position: absolute; 
-							margin: 0; 
-							width: 100%; 
-							height: 1px; 
-							background: black;
-							border: none;
-						}
-					`],
+		hr {
+			position: absolute; 
+			margin: 0; 
+			width: 100%; 
+			height: 1px; 
+			background: black;
+			border: none;
+		}
+	`],
 	template: `
-						@if (report) {
-							<div style="position: relative; min-height: 101px; width: 3ch; margin-left: auto; margin-right: auto;">
-								<hr style="top: 0">
-								<hr style="top: 100px">
-								@if (report.maxAt) {
-									<hr style="top: {{report.maxAt * 100}}px">
-								}
-								<div style="margin-left: 2px; margin-right: 2px"
-									[style.height]="report.coverage * 100 + 'px'"
-									[style.background-color]="color"
-								></div>
-							</div>
-						}
-						`
+		@if (report) {
+			<div style="position: relative; min-height: 101px; width: 3ch; margin-left: auto; margin-right: auto;">
+				<hr style="top: 0">
+				<hr style="top: 100px">
+				@if (report.maxAt) {
+					<hr style="top: {{report.maxAt * 100}}px">
+				}
+				<div style="margin-left: 2px; margin-right: 2px"
+					[style.height]="report.coverage * 100 + 'px'"
+					[style.background-color]="color"
+				></div>
+			</div>
+		}
+	`
 })
 export class CoverageIndicatorComponent {
 	@Input()
